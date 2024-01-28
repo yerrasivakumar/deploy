6120b3d3006287ee73744c4cf962dc80d29bec93
yerrasivakumar143 <yerrasiva123456@gmail.com>
2024-01-28 11:55:14 +0530

    fourth

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ dbHOST ='mongodb+srv://siva:siva@cluster1.qaavblr.mongodb.net/?retryWrites=true&
 mongoose.connect(dbHOST).then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error connecting to MongoDB:', err));
 app.use(express.json());
-var corsOptions = {
-    origin: 'https://deploy-five-kappa.vercel.app',
-    methods:["GET","POST"],
-    Credential:true,
-    // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-  
-  app.use(cors(corsOptions));
+app.use(cors())
 app.use(bodyParser.json());
 PORT = 7000
 app.use('/user', userRoutes);
