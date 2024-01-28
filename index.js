443a6629425ed362925f187345d1989d8f199dfa
yerrasivakumar143 <yerrasiva123456@gmail.com>
2024-01-28 11:41:09 +0530

    third

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express();
 const userRoutes = require('./routes/userrouter');
 const mongoose = require('mongoose')
-
+var cors = require('cors')
 const bodyParser = require('body-parser');
 dbHOST ='mongodb+srv://siva:siva@cluster1.qaavblr.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(dbHOST).then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error connecting to MongoDB:', err));
 app.use(express.json());
+var corsOptions = {
+    origin: 'https://deploy-five-kappa.vercel.app',
+    methods:["GET","POST"],
+    Credential:true,
+    // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+  
+  app.use(cors(corsOptions));
 app.use(bodyParser.json());
 PORT = 7000
 app.use('/user', userRoutes);
